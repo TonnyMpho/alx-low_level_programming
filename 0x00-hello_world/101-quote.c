a7a154e9a37c81b9dbe3006f865dd9785b0d0ad7
TonnyMpho <tonnympho1@gmail.com>
2023-03-17 02:46:09 -0700

    C programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *  C program that prints exactly
+ *  "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *  Return: Always 1 (error)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
