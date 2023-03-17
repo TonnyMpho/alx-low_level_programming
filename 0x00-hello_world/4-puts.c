e1ef9e1c1107a64ce4386edd1cf59788a217e338
TonnyMpho <tonnympho1@gmail.com>
2023-03-16 23:03:50 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle, followed by a new line");
+	puts("\"Programming is like building a multilingual puzzle, followed by a new line\n");
 	return (0);
 }
