41cea473f03ca7f3a152bb88a0dcae8582815c1f
TonnyMpho <tonnympho1@gmail.com>
2023-03-17 09:06:24 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-	char alphabet = 'a';
+	char alphabet;
 
-	for (alphabet; alphabet <= 'z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		putchar(alphabet);
 	}
 
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
