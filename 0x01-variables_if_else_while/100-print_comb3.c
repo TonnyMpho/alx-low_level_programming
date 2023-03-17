765bae8f7c936600fc2d1af0e24f46049f044ac1
TonnyMpho <tonnympho1@gmail.com>
2023-03-17 16:09:46 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,7 +15,7 @@ int main(void)
 	for (digits_1 = 0; digits_1 < 10; digits_1++)
 	{
 		digits_2 = digits_1++;
-		if (digits_1 < 9 && digits_2 < 10)
+		if (digits_1 < 10 && digits_2 < 10)
 		{
 			putchar(digits_1 + 48);
 			putchar(digits_2 + 48);
