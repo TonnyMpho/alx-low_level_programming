24b15cebb74935a5f79a322d8c7376c8f7cc2ea4
TonnyMpho <tonnympho1@gmail.com>
2023-03-16 21:07:08 -0700

    C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * C program that prints exactly 
+ * "Programming is like building a multilingual puzzle, followed by a new line.
+ * Using the function puts
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle, followed by a new line");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * C program that prints exactly
+ * \"with proper grammar, but the outcome is a piece of art.\"
+ * Using the function puts
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
