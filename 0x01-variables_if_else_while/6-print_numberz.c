56e100221dd3e06f9dd532b8fa894a1c07ecb218
TonnyMpho <tonnympho1@gmail.com>
2023-03-17 14:33:47 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,7 +5,7 @@
  * A program that prints all single digit numbers
  * of base 10 starting from 0, followed by a new line.
  * You can only use the putchar function
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
