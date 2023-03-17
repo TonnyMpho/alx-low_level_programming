bf537e98187d4fbc418fff3151c9821bf1a34741
TonnyMpho <tonnympho1@gmail.com>
2023-03-16 23:15:04 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /*
- * C program that prints exactly 
- * "Programming is like building a multilingual puzzle, followed by a new line.
- * Using the function puts
- */
+* C program that prints exactly 
+* \"Programming is like building a multilingual puzzle, followed by a new line.
+* Using the function puts
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
