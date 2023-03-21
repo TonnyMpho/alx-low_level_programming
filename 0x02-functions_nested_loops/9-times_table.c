a6d5f040fe70d76c77fc21de292eadc56298bc0e
TonnyMpho <tonnympho1@gmail.com>
2023-03-21 06:06:05 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,14 +11,14 @@ void times_table(void)
 
 	while (a <= 9)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 1; b <= 9; b++)
 		{
 			multi = a * b;
 
 			if (multi > 9)
 				_putchar((multi / 10) + '0');
 
-			if (multi < 9)
+			if (multi <= 9)
 				_putchar(' ');
 
 			_putchar((multi % 10) + '0');
