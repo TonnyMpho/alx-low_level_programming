d65113f687764b901b376a1521902bce2e39296d
TonnyMpho <tonnympho1@gmail.com>
2023-03-21 02:34:27 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * ack_bauer - Function
+ * jack_bauer - Function
  * A function that prints every minute of the day of
  * Jack Bauer, starting from 00:00 to 23:59.
  * Return: nothing Void
  */
 void jack_bauer(void)
 {
-	int hours = 23;
-	int minutes = 59;
+	int hours = 0;
+	int minutes = 0;
 
 	while (hours <= 23)
 	{
-		while (minutes <=59)
+		while (minutes <= 59)
 		{
 			_putchar((hours / 10) + 48);
 			_putchar((hours % 10) + 48);
