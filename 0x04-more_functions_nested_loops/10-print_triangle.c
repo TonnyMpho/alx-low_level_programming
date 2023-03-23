4456fedd13607a20f288c64d706950d585e04514
TonnyMpho <tonnympho1@gmail.com>
2023-03-23 14:22:24 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - Function that prints a triangle, followed by a new line.
+ *@size: size of the triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int n_row, hash, s;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (n_row = 1; n_row <= size; n_row++)
+		{
+			for (s = size - n_row; s >= 1; s--)
+			{
+				_putchar(' ');
+			}
+			for (hash = 1; hash <= row; h++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
