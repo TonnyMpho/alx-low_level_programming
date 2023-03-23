4143bab016a6359a9b72c5ee88745ac9818e1275
TonnyMpho <tonnympho1@gmail.com>
2023-03-23 05:15:33 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_numbers - Write a function that prints the numbers,
+ * print_most_numbers - Write a function that prints the numbers,
  * from 0 to 9, followed by a new line.
  * Return: numbers
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	for(int a = 0; a < 10; a++)
 		if (a != 2 && a != 4)
