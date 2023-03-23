2c5e0cc8d1bbcbf0814a0253844dbfaa5482d114
TonnyMpho <tonnympho1@gmail.com>
2023-03-23 06:16:07 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,9 +10,9 @@ void print_most_numbers(void)
 {
 	int a;
 
-	for(a = 0; a < 10; a++)
+	for(a = '0'; a < '10'; a++)
 	{
-		if ((a != 2) && (a != 4))
+		if ((a != '2') && (a != '4'))
 		{
 			_putchar(a);
 		}
