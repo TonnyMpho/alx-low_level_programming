5b74f345a8368509c7494b2110fadefb955c737b
TonnyMpho <tonnympho1@gmail.com>
2023-03-23 05:53:14 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - Write a function that prints the numbers,
+ * print_numbers - Write a function that
+ * prints the numbers,
  * from 0 to 9, followed by a new line.
  * Return: numbers
  */
@@ -11,7 +12,9 @@ void print_numbers(void)
 
 	for(a = 0; a < 10; a++)
 	{
+
 		_putchar(a + '0');
+
 	}
 
 	_putchar('\n');
