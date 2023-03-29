1e482e3c1bcb39d9ee6624d0a722a51a06293560
TonnyMpho <tonnympho1@gmail.com>
2023-03-29 06:54:52 -0700

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcmp - A function that compares two strings.
+ * @s1: pointer to first string
+ * @s2: pointer to second string
+ *
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int comp, i = 0;
+
+	while (s1[i] != '\0' && s1 == s2)
+		i++;
+
+	comp = s1[i] - s2[i];
+
+	return comp;
+}
