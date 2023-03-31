7260b1c7786add7238ea086c683f020be489ba9b
TonnyMpho <tonnympho1@gmail.com>
2023-03-31 11:05:06 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -4,7 +4,8 @@
  * infinite_add - A function that adds two numbers.
  * @n1: first pointer to check for the first number
  * @n2: secont number
- * @r: is the buffer that the function will use to store the result
+ * @r: is the buffer that the function will use to
+ * store the result
  * @size_r: is the buffer size
  *
  * Return: The function returns a pointer to the result
