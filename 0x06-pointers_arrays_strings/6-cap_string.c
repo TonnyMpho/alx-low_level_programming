9fd8e1903e8f69dfbc1699666e167bc46f6edde7
TonnyMpho <tonnympho1@gmail.com>
2023-03-30 19:59:48 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,31 +7,24 @@
  */
 char *cap_string(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
-		if(i == 0)
+		if (str[0] >= 'a' && str[0] <= 'z')
 		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-				str[i] = str[i] - 32;
-			
-			continue;
+			str[0] = str[0] - 32;
 		}
-		if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t')
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+		    || str[i] == ',' || str[i] == ';' || str[i] == '.'
+		    || str[i] == '.' || str[i] == '!' || str[i] == '?'
+		    || str[i] == '"' || str[i] == '(' || str[i] == ')'
+		    || str[i] == '{' || str[i] == '}')
 		{
-			++i;
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] = str[i + 1] - 32;
 		}
-		else
-		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-				str[i] = str[i] + 32;
-		}
+		i++;
 	}
 	return (str);
 }
