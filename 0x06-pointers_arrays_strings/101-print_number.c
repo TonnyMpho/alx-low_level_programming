39c59a9cbc5eb0e044c0d577c87002c7c8d6efc0
TonnyMpho <tonnympho1@gmail.com>
2023-03-31 01:39:01 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,17 +4,17 @@
  * print_number - A function that prints an integer.
  * @n: number to be printed
  * Return: void
- */
+ *i/
 void print_number(int n)
 {
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n = -n;
 	}
 
 	if ((n / 10) > 0)
 		print_number(n / 10);
 
-	putchar((n % 10) + '0');
+	_putchar((n % 10) + '0');
 }
