e384894f70ffb33bb672640c54393c4e33d4f4c2
TonnyMpho <tonnympho1@gmail.com>
2023-04-01 09:56:04 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -19,7 +19,9 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		for (j = 0; n2[j]; j++)
 		{
 			if (i > size_r || j > size_r)
+			{
 				return (0);
+			}
 		}
 	}
 
@@ -33,10 +35,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		{
 			n += n1[i] - '0';
 		}
+
 		if (j >= 0)
 		{
 			n += n2[j] - '0';
 		}
+
 		if (i < 0 && j < 0 && n == 0)
 		{
 			break;
@@ -45,11 +49,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		m = n / 10;
 		r[k] = n % 10 + '0';
 	}
-
 	r[k] = '\0';
 
 	if (i >= 0 || j >= 0 || m)
+	{
 		return (0);
+	}
 
 	for (k -= 1, l = 0; l < k; k--, l++)
 	{
