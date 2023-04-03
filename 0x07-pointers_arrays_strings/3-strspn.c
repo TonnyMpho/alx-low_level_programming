0d7453b289690e2e88b5d6a820e434423bdc6e72
TonnyMpho <tonnympho1@gmail.com>
2023-04-03 16:38:20 -0700

    0x07. C - Even more pointers, arrays and strings0x07.

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, length;
-	
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
@@ -29,5 +29,6 @@ unsigned int _strspn(char *s, char *accept)
 			return (length);
 		}
 	}
+
 	return (i);
 }
