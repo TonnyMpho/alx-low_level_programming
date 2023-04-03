08835c91ea9b4658db056dde218383d18eb482ac
TonnyMpho <tonnympho1@gmail.com>
2023-04-03 16:11:12 -0700

    0x07. C - Even more pointers, arrays and strings0x07.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function that searches a string for any of a set of bytes.
+ * @s: string to match from
+ * @accept: pointer the string/bytes to be matched
+ *
+ * Return: Returns a pointer to the byte in s that matches
+ * one of the bytes in accept, or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int a, b;
+	char *p;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; accept[b] != '\0'; b++)
+		{
+			if (s[a] == accept[b])
+			{
+				p = &s[a];
+				return (p);
+			}
+		}
+	}
+
+	return NULL;
+}
