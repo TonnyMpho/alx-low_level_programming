60b802d469ff83fe9b9ff51a432fc6c341665b31
TonnyMpho <tonnympho1@gmail.com>
2023-04-05 20:13:05 -0700

    0x08. C - Recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,7 +5,6 @@
  * input integer is a prime number, otherwise return 0
  *
  * @n: number
- * @d: divisor
  * Return: prime or not
  */
 int prime_check(int n, int d);
@@ -20,6 +19,9 @@ int is_prime_number(int n)
 
 /**
  * prime_check - check to see if it a prime or not
+ * @n: number
+ * @d: divider/divisor
+ * Return: prime
  */
 int prime_check(int n, int d)
 {
