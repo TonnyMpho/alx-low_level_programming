2a3ab7bf9491f012ee647a78cdd8a880f76f4758
TonnyMpho <tonnympho1@gmail.com>
2023-04-13 06:18:40 -0700

    0x0B. C - malloc, free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned int s1len, s2len;
+	unsigned int s1_length, s2_length;
 	unsigned int i, j;
 	i = 0;
 	j = 0;
@@ -22,23 +22,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	s1len = 0;
-	while (s1[s1len] != '\0')
-		s1len++;
+	s1_length = 0;
+	while (s1[s1_length] != '\0')
+		s1_length++;
 
-	s2len = 0;
-	while (s2[s2len] != '\0')
-		s2len++;
+	s2_length = 0;
+	while (s2[s2_length] != '\0')
+		s2_length++;
 
-	if (n >= s2len)
-		n = s2len;
+	if (n >= s2_length)
+		n = s2_length;
 
-	str = malloc(sizeof(*str) * (s1len + n + 1));
+	str = malloc(sizeof(*str) * (s1_length + n + 1));
 
 	if (str == NULL)
 		return (NULL);
 
-	while (i < s1len)
+	while (i < s1_length)
 	{
 		str[i] = s1[i];
 		i++;
@@ -46,11 +46,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	while (j < n)
 	{
-		str[s1len + j] = s2[j];
+		str[s1_length + j] = s2[j];
 		j++;
 	}
 
-	str[s1len + n] = '\0';
+	str[s1_length + n] = '\0';
 
 	return (str);
 }
