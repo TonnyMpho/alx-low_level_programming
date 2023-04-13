2f1f36bb2ac558c0b25a5ee17971691518560bce
TonnyMpho <tonnympho1@gmail.com>
2023-04-13 05:49:00 -0700

    0x0B. C - malloc, free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,10 +17,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	j = 0;
 
 	if (s1 == NULL)
-		s1 == "";
+		s1 = "";
 
 	if (s2 == NULL)
-		s2 == "";
+		s2 = "";
 
 	s1len = 0;
 	while (s1[s1len] != '\0')
