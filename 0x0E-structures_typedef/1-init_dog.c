f9efe41dfef196a8cfe873ac89b07e70bce9fa99
TonnyMpho <tonnympho1@gmail.com>
2023-04-17 20:51:10 -0700

    0x0E. C - Structures, typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,7 +11,26 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	int i = 0, j = 0;
+
+	if (d == NULL)
+		exit (1);
+
+	while (name[i] != '\0')
+	{
+		d->name[i] = name[i];
+		i++;
+	}
+	d->name[i] = '\0';
 	d->name = name;
+
 	d->age = age;
+
+	while (owner[j] != '\0')
+	{
+		d->owner[j] = owner[j];
+		j++;
+	}
+	d->owner[j] = '\0';
 	d->owner = owner;
 }
