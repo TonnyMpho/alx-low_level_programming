acbaf23834171a37896714f79e3fae24771d9ba2
TonnyMpho <tonnympho1@gmail.com>
2023-04-22 06:48:24 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,65 +2,55 @@
 
 /**
  * op_add - Adds two integers
- *
  * @a: Integer a
  * @b: Integer b
- *
  * Return: The sum of a and b
  */
 int op_add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
 
 /**
  * op_sub - Subtracts two integers
- *
  * @a: Integer a
  * @b: Integer b
- *
  * Return: The difference of a and b
  */
 int op_sub(int a, int b)
 {
-    return (a - b);
+	return (a - b);
 }
 
 /**
  * op_mul - Multiplies two integers
- *
  * @a: Integer a
  * @b: Integer b
- *
  * Return: The product of a and b
  */
 int op_mul(int a, int b)
 {
-    return (a * b);
+	return (a * b);
 }
 
 /**
  * op_div - Divides two integers
- *
  * @a: Integer a
  * @b: Integer b
- *
  * Return: The result of the division of a & b
  */
 int op_div(int a, int b)
 {
-    return (a / b);
+	return (a / b);
 }
 
 /**
  * op_mod - modulus of two integers
- *
  * @a: first number
  * @b: second number
- *
  * Return: The remainde
  */
 int op_mod(int a, int b)
 {
-    return (a % b);
+	return (a % b);
 }
