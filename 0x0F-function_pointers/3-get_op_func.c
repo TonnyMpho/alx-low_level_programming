5bb077e48c4f357600dc4403ebd1bc2e21efb5f2
TonnyMpho <tonnympho1@gmail.com>
2023-04-22 06:16:42 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (*(ops[i].op) == *s && s[i] != '\0')
+		if (ops[i].op != *s && s[i] != '\0')
 			return (ops[i].f);
 		i++;
 	}
