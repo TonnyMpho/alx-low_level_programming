5ec47eadec471d8e30a9d7dd0e72be5011518da2
TonnyMpho <tonnympho1@gmail.com>
2023-04-23 11:54:20 -0700

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs.
+ * @d: the dog struct to be freed
+ * Return: Void - Nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
