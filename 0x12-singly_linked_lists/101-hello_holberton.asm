a31559e95f2e094a9e180b2145511a3b230af603
TonnyMpho <tonnympho1@gmail.com>
2023-04-28 13:20:32 -0700

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,14 @@
 section .data
-	message db "Hello, Holberton", 0xA, 0
+	message db "Hello, Holberton", 10, 0
 
 section .text
 	global main
 	extern printf
 
 main:
-	push message
+	push message 
 	call printf ;call printf
 	add rsp, 8 ;resert the stack
 
-	xor, eax, eax
+	xor eax, eax
 	ret
