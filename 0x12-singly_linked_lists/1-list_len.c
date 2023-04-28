71f481c6dfe91b92cb5fcafd69d9f31b1b7a9aed
TonnyMpho <tonnympho1@gmail.com>
2023-04-28 10:57:45 -0700

    0x12. C - Singly linked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements in a linked list_t list.
+ * @h: head of the list
+ * Return: number of elements in a linked list_t list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h != NULL)
+		elements++;
+
+	return (elements);
+}
