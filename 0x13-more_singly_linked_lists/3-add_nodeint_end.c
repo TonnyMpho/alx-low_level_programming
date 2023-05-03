3faba0ae4d0a62daf41596d691e809940e283e64
TonnyMpho <tonnympho1@gmail.com>
2023-05-03 07:01:32 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,7 +2,8 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - function that adds a new node at the end of a listint_t list.
+ * add_nodeint_end - function that adds a new node at the end
+ * of a listint_t list.
  * @head: haed of the linked list
  * @n: element to add to the list
  * Return: the address of the new element, or NULL if it failed
