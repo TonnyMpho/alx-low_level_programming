4e11edc881620b47f4037ed9597c12e114cca472
TonnyMpho <tonnympho1@gmail.com>
2023-05-03 07:00:08 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,8 +9,8 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t n_node = malloc(sizeof(listint_t));
-	listint_t node = *head;
+	listint_t *n_node = malloc(sizeof(listint_t));
+	listint_t *node = *head;
 
 	if (n_node == NULL)
 		return (NULL);
