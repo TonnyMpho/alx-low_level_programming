d72b352dcf45701a882817652acda9c492b3f6b7
TonnyMpho <tonnympho1@gmail.com>
2023-05-03 20:58:32 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 	prev_node = *head;
-	curr_node = prev-node->next;
+	curr_node = prev_node->next;
 
 	while (i < index && curr_node != NULL)
 	{
