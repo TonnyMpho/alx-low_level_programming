4692357869511e812cb84851ab3fa10ee0edad57
TonnyMpho <tonnympho1@gmail.com>
2023-05-25 16:20:43 -0700

    0x15-file_io

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -240,7 +240,7 @@ void print_entry(unsigned long int e_entry, unsigned char *eident)
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *header;
-	int o = open(argv[1], O_RDONLY), r;
+	int o = open(argv[1], O_RDONLY);
 
 	if (o == -1)
 	{
@@ -254,8 +254,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
 	}
-	r = read(o, header, sizeof(Elf64_Ehdr));
-	if (r == -1)
+	if (read(o, header, sizeof(Elf64_Ehdr)) == -1)
 	{
 		free(header);
 		close(o);
