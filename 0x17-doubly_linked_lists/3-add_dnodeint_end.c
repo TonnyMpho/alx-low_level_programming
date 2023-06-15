eb38df89b2f6eb86a922e95aa15384dfa1a5ce73
TonnyMpho <tonnympho1@gmail.com>
2023-06-15 16:49:56 -0700

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -24,7 +24,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	if (*head != NULL)
 	{
-		while ((*head)-next != NULL)
+		while ((*head)->next != NULL)
 			*head = (*head)->next;
 
 		node->prev = *head;
