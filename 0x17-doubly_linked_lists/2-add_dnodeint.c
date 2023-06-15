901ba50b98a2a9f1a241670311ced2ae790c152d
TonnyMpho <tonnympho1@gmail.com>
2023-06-15 16:14:24 -0700

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,8 +13,8 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *node;
-	node = malloc(sizeof(dlistint_t));
 
+	node = malloc(sizeof(dlistint_t));
 	if (node == NULL)
 		return (NULL);
 
