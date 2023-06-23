372153e737e66b317e487227f97357f8094647a8
TonnyMpho <tonnympho1@gmail.com>
2023-06-23 15:49:11 -0700

    0x18. C - Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 wget -P /tmp https://github.com/TonnyMpho/alx-low_level_programming/raw/88ac33d0e86f5e4735568b5fca7aa7d9aea57197/0x18-dynamic_libraries/libgm.so
 export LD_PRELOAD=/tmp/libgm.so
