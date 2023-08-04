fb8801770ee323b33bb58399b0c1847c8ad15c34
TonnyMpho <tonnympho1@gmail.com>
2023-08-04 07:53:44 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,5 +7,23 @@
  */
 int main()
 {
+	long int num = 612852475143;
+	long int prim = 2;
+
+	while (prim <= num)
+	{
+		if (num % prim == 0)
+		{
+			num /= prim;
+			continue;
+		}
+
+		if (prim == 2)
+			prim = 3;
+		else
+			prim += 2;
+	}
+
+	printf("%li\n", num);
 	return (0);
 }
