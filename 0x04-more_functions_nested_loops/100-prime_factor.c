76543975051eb0385b86f1007080cb926c067439
TonnyMpho <tonnympho1@gmail.com>
2023-08-04 08:00:01 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 
-/*
- * main - main entry point
+/**
+ * main - entry point
  *
  * Return: Always 0
  */
-int main()
+int main(void)
 {
 	long int num = 612852475143;
 	long int prim = 2;
 
-	while (prim <= num)
+	while (prim * prim <= num)
 	{
 		if (num % prim == 0)
 		{
 			num /= prim;
-			continue;
 		}
-
-		if (prim == 2)
-			prim = 3;
 		else
-			prim += 2;
+		{
+			if (prim == 2)
+				prim = 3;
+			else
+				prim += 2;
+		}
 	}
 
 	printf("%li\n", num);
