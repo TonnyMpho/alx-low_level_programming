220baa021ec56e84f950737ab9f79398c169e326
TonnyMpho <tonnympho1@gmail.com>
2023-08-04 08:25:27 -0700

    0x06. C - More pointers, arrays and strings

---
+++ 0x04-more_functions_nested_loops/104-print_buffer.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * print_buffer - function that prints a buffer
+ * @b: buffer
+ * @size: size of buffer
+ * Return: Void (Nothing)
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j;
+	char c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+				printf("%02x ", b[i + j]);
+			else
+				printf("   ");
+		}
+
+		printf(" ");
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				c = b[i + j];
+				if (isprint(c))
+				{
+					putchar(c);
+				}
+				else
+				{
+					putchar('.');
+				}
+			}
+		}
+
+		putchar('\n');
+	}
+}
