6cbd0c8bfaf02d4420e74bb8fec27438be67f3af
TonnyMpho <tonnympho1@gmail.com>
2023-08-23 13:44:18 -0700

    C - Makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Island Perimeter Module """
+
+def island_perimeter(grid):
+    """
+    function that returns the perimeter of the island described in grid
+    """
+    perimeter = 0
+
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
